2f371ce72e0538d6b8dac3eed03d12cc15f14907
leroyg <leroy.gardner@gmail.com>
2019-08-13 20:41:57 -0500

    initial branch

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+//express.json is a function (method) that returns middleware
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
